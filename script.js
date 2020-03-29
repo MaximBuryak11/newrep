8203b5d843829f849cab8082c25f0122c40762db
Maxim Buryak <mx.buryak@gmail.com>
2020-03-29 22:13:43 +0500

    third commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+let money; 
+let income; 
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+alert('Привет, Мир');
+
+console.log('Привет из Консоли!');
+console.log('Hello World');
