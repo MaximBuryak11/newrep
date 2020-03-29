1cd6a10e9c49e89163ec6fab386d8207d4561d28
Maxim Buryak <mx.buryak@gmail.com>
2020-03-29 23:31:22 +0500

    fifth commit

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ let deposit;
 let mission;
 let period;
 
-alert('Привет, Мир');
-console.log('Привет из Консоли!');
+money = 100;
+income = 20;
+addExpenses = 'Интернет, такси, коммуналка';
+deposit = true;
+mission = 1000;
+period = 11;
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+let budgetDay;
+budgetDay = money/30;
+console.log(budgetDay);
